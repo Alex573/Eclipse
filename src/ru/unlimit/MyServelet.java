6273b8f29da1ff70584b293ce85ae88a1d5cc6e4
Lex <alexreferal@gmail.com>
2018-01-13 19:09:22 +0100

    3 ukola

--- src/ru/unlimit/MyServelet.java
+++ src/ru/unlimit/MyServelet.java
@@ -2,7 +2,7 @@ package ru.unlimit;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,12 +22,61 @@ public class MyServelet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String name  = request.getParameter("firstname");
+		String lastname  = request.getParameter("lastname");
+		String[] jobs  = request.getParameterValues("job");
+		String gender  = request.getParameter("gender");
+		if (gender==null) {
+				gender="-"; 
+		}
+		String age18  = request.getParameter("age18");
+		if (age18==null) {
+			age18="-"; 
+	}
+		
+		
+		
+		
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		response.setContentType("text/html;charset=UTF8");
 		//response.setCharacterEncoding("UTF-8");
 		
 		PrintWriter out = response.getWriter();
-		out.println("<h3>Hello from efgěšč Валд</h3><br>Agaain</br>");
+		out.println("<h3>People</h3>");
+		out.println("Name: "+name+"<br>");
+		out.println("Family: "+lastname+"<br>");
+		out.println("Job: "+Arrays.deepToString(jobs)+"<br>");
+		 
+		out.println("Gender: "+gender+"<br>");
+		out.println("Age18: "+age18+"<br>");
+		
+		out.close();
+	}
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		String name  = request.getParameter("firstname");
+		String lastname  = request.getParameter("lastname");
+		String job  = request.getParameter("job");
+		String gender  = request.getParameter("gender");
+		String age18  = request.getParameter("age18");
+		
+		
+		
+		
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF8");
+		//response.setCharacterEncoding("UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<h3>People</h3>");
+		out.println("Name: "+name+"<br>");
+		out.println("Family: "+lastname+"<br>");
+		out.println("Job: "+job+"<br>");
+		out.println("Gender: "+gender+"<br>");
+		out.println("Age18: "+age18+"<br>");
+		
 		out.close();
 	}
 
